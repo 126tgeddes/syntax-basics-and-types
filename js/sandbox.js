b5e759c87ee168b85afcf6d14bffad5e012aea97
Timmy <126tgeddes@frhsd.com>
2024-10-10 12:43:08 -0400

    10/10/24

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,14 +1,22 @@
-let age = 25;
-let year = 2019;
+let ninjas = ['shaun', 'ryu', 'chun-li'];
 
+// ninjas[1] = 'ken';
+// console.log(ninjas[1]);
 
-console.log(age, year);
+// let ages = [20,25,30,35];
+// console.log(ages[2]);
 
-age = 30;
-console.log(age);
+// let random = ['shaun', 'crystal', 30, 20];
+// console.log(random);
 
-const points = 100;
-console.log(points);
+//console.log(ninjas.length);
 
-var score = 75;
-console.log(score);
+// array methods
+
+//let results = ninjas.join('-');
+//let results = ninjas.indexOf('chun-li');
+//let results = ninjas.concat(['ken', 'crystal']);
+let result = ninjas.push('ken');
+result = ninjas.pop();
+
+console.log(result);
